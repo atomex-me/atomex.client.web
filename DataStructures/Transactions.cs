44a59526e39df044b177afdbb252849ba01fbe69
Konstantin <johnysalvesen@gmail.com>
2020-02-27 20:28:22 +0300

    Wallet Data Table component WIP: todo:description

--- DataStructures/Transactions.cs
+++ DataStructures/Transactions.cs
@@ -5,20 +5,22 @@ using Atomex.Core;
 
 namespace atomex_frontend.atomex_data_structures
 {
-  class Transaction : IBlockchainTransaction
+  public class Transaction : IBlockchainTransaction
   {
     public Transaction(
       string id,
       BlockchainTransactionState state,
       BlockchainTransactionType type,
       DateTime? creationTime,
-      bool isConfirmed)
+      bool isConfirmed,
+      double amount)
     {
       Id = id;
       State = state;
-      Type = Type;
-      CreationTime = CreationTime;
+      Type = type;
+      CreationTime = creationTime;
       IsConfirmed = isConfirmed;
+      Amount = amount;
     }
 
     public string Id { get; set; }
@@ -26,6 +28,8 @@ namespace atomex_frontend.atomex_data_structures
     public BlockchainTransactionType Type { get; set; }
     public DateTime? CreationTime { get; set; }
     public bool IsConfirmed { get; set; }
+
+    public double Amount { get; set; }
 
     public Currency Currency => throw new NotImplementedException();
     public BlockInfo BlockInfo => throw new NotImplementedException();
